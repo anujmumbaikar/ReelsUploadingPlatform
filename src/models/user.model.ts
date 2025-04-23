ab8479fe959cd3c8a56edc7627ac3a04d4a7accc
Anuj Mumbaikar <anujmumbaikar120405@gmail.com>
2025-04-23 15:18:11 +0530

    Add user model with schema and password hashing

---
+++ src/models/user.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema,model,models } from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface IUser{
+    email:string;
+    password:string;
+    _id?:mongoose.Types.ObjectId;
+    createdAt?:Date;
+    updatedAt?:Date;
+}
+const userSchema = new Schema<IUser>(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+userSchema.pre("save",async function(next){
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+})
+const User = mongoose.models?.User || mongoose.model<IUser>("User", userSchema);
+export default User;
